7e6b256a86c1e258f8a384f3f08cffc60f0a5ad9
Joe Calvillo <jcalvillo757@gmail.com>
2016-07-16 16:45:26 -0400

    local host test

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,4 +15,4 @@ posts = Post.create([{title:"First post",description:"This is the first descript
 comments = Comment.create([{comment:"Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi.",category_id:1,post_id:1,user_id:1},
 {comment:"Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia.",category_id:1,post_id:2,user_id:2},
 {comment:"Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.",category_id:2,post_id:3,user_id:3},
-{comment:"Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat. Praesent blandit.",category_id:2,post_id:4,user_id:4}]);
+{comment:"Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat. Praesent blandit.",category_id:2,post_id:4,user_id:4}]); 
