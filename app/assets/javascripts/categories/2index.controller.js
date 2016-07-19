302d5d591278329b21d4ec8258e218d166038c2b
Joe Calvillo <jcalvillo757@gmail.com>
2016-07-19 12:15:23 -0400

    Trying to push categories

--- app/assets/javascripts/categories/2index.controller.js
+++ app/assets/javascripts/categories/2index.controller.js
@@ -3,31 +3,38 @@
 (function(){
   angular
   .module("categories", ["ngResource"])
-  .controller("category_controller", ["$resource", CategoryController]);
+  .controller("category_controller", [
+    "CategoryFactory",
+    "$resource",
+    "$stateParams",
+    CategoryController
+  ]);
 
-  function CategoryController($resource){
+  function CategoryController(CategoryFactory, $resource, $stateParams){
     var vm = this;
     var Category = $resource("/categories/:id.json", {}, {
       update: {method: "PUT"}
     });
-    vm.cat_data = Category.query();
+    vm.cat_data = Category.query(function(response){});
+
 
     vm.sort_cat_data_by = function(name){
       vm.sort_on = name;
       vm.is_descending = !(vm.is_descending);
     }
 
-    vm.destroy = function(category_index){
-      var category = vm.cat_data[category_index];
-      Category.remove({id: category.id}, function(response){
-        if(response.success) vm.cat_data.splice(category_index, 1);
-      });
+    vm.destroy = function(category){
+      console.log(vm.category)
+      vm.category.remove(category);
     }
 
     vm.new_category = {};
     vm.create = function(){
+      console.log(vm.new_category)
       Category.save(vm.new_category, function(response){
+        console.log(vm.cat_data)
         vm.cat_data.push(response);
+        console.log(vm.cat_data)
         vm.new_category = {};
       });
     }
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -28,18 +28,9 @@ class CategoriesController < ApplicationController
   # POST /categories
   # POST /categories.json
   def create
-    @category = Category.new(category_params)
-    @category.save
+    @category = Category.create(category_params)
 
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
+    render json: @product, status: :ok
   end
 
   # PATCH/PUT /categories/1
