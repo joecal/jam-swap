bc9941309acea1db2ce6f8345e39ace5a2a8da14
Joe Calvillo <jcalvillo757@gmail.com>
2016-07-20 09:38:26 -0400

    Pushing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716214734) do
+ActiveRecord::Schema.define(version: 20160720131205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,42 +21,20 @@ ActiveRecord::Schema.define(version: 20160716214734) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.string   "comment"
-    t.integer  "user_id"
-    t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
-    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
-  end
-
-  create_table "posts", force: :cascade do |t|
+  create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "email"
-    t.string   "phone"
-    t.string   "street"
-    t.string   "city"
-    t.string   "state"
-    t.string   "picture_url"
-    t.integer  "category_id"
-    t.integer  "user_id"
+    t.string   "url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["category_id"], name: "index_posts_on_category_id", using: :btree
-    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "meetups", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "posts", "categories"
-  add_foreign_key "posts", "users"
 end
