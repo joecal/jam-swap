1ef6b9fbb32c525b10e98ad418c79171f32d4aac
Adrian Maseda <amaseda@gmail.com>
2016-08-03 12:51:17 -0400

    first round of comments

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,3 +1,5 @@
+# AM: What is the purpose of the "Home" model?
+
 class Home < ApplicationRecord
 
 end
--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,5 +1,6 @@
 class Meetup < ApplicationRecord
   has_one :category
   has_one :user
+  # AM: Good job using a single model to handle comments that belong to different models. Keeps the schema clean!
   has_many :comments
 end
